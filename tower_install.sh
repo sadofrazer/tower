4dd8a8bac03eb651817773c60238334274d9ddb7
sadofrazer <83238471+sadofrazer@users.noreply.github.com>
2022-01-04 21:22:58 +0100

    add install script of docker and docker-compose

--- tower_install.sh
+++ tower_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ubuntu
+#Install Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/sadofrazer/tower.git
 cd cursus-devops/tower/
 tar -xzvf awx.tar.gz -C ~/
