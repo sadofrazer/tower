7887788213cd4dba0ab21e761c65412e3e289519
Frazer SADO <83238471+sadofrazer@users.noreply.github.com>
2023-01-29 12:45:40 +0100

    change the version of docker-compose

--- tower_install.sh
+++ tower_install.sh
@@ -6,7 +6,8 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker ubuntu
 #Install Docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.1/docker-compose-linux-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/sadofrazer/tower.git
 cd tower/
